ba1e7ec81857dac2d41cf77327fb7b14684b7abe
linda_a <larmstrongnz@gmail.com>
2025-02-21 15:12:22 +1300

    Edited indentation

--- api/api.py
+++ api/api.py
@@ -1,4 +1,3 @@
-import time
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -15,6 +14,7 @@ class Recipe(db.Model):
     description = db.Column(db.Text, nullable=True, default='Delicious. You need to try it!')
     image_url = db.Column(db.String(500), nullable=True, default="https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
     servings = db.Column(db.Integer, nullable=False)
+    
     def __repr__(self):
         return f"Recipe(id={self.id}, title='{self.title}', descritpion='{self.description}', servings={self.servings})"
 
@@ -42,37 +42,37 @@ def get_all_recipes():
 # Route to add a new recipe
 @app.route('/api/recipes', methods=['POST'])
 def add_recipe():
-data=request.get_json()
-# Validate the incoming JSON data for required fields
-required_fields = ['title', 'ingredients', 'instructions', 'servings', 'descritpion', 'image_url']
+    data = request.get_json()
+    # Validate the incoming JSON data for required fields
+    required_fields = ['title', 'ingredients', 'instructions', 'servings', 'descritpion', 'image_url']
     
-for field in required_fields:
-    if field not in data or data[field] == "":
-        return jsonify({'error': f"Missing required field: '{field}'"}), 400
+    for field in required_fields:
+        if field not in data or data[field] == "":
+            return jsonify({'error': f"Missing required field: '{field}'"}), 400
         
-new_recipe = Recipe(
+    new_recipe = Recipe(
         title=data['title'],
-        ingredients=data['ingredients'],
+        ingredients=data['ingredients'],    
         instructions=data['instructions'],
         servings=data['servings'],
         description=data['description'],
         image_url=data['image_url']
     )
     
-db.session.add(new_recipe)
-db.session.commit()
+    db.session.add(new_recipe)
+    db.session.commit()
 
-# Serialize the new recipe and return in as JSON
-new_recipe_data = {
-    'id': new_recipe.id,
-    'title': new_recipe.title,
-    'ingredients': new_recipe.ingredients,
-    'servings': new_recipe.servings,
-    'description': new_recipe.descritpion,
-    'image_url': new_recipe.image_url
-}
+    # Serialize the new recipe and return in as JSON
+    new_recipe_data = {
+        'id': new_recipe.id,
+        'title': new_recipe.title,
+        'ingredients': new_recipe.ingredients,
+        'servings': new_recipe.servings,
+        'description': new_recipe.descritpion,
+        'image_url': new_recipe.image_url
+    }
 
-return jsonify({'message':'Recipe added successfully', 'recipe':new_recipe_data})
+    return jsonify({'message':'Recipe added successfully', 'recipe':new_recipe_data})
 
 
 if __name__ == '__main__':
